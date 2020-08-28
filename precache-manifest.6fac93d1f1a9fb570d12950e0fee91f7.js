697270f7790f83fe5bbfc05ed77f6aee5a2cf6e6
apoorvdwi <apoorvd14@gmail.com>
2020-08-28 22:20:26 +0530

    Updates

---
+++ precache-manifest.6fac93d1f1a9fb570d12950e0fee91f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af172a6afc559940d3b79b012cd8237f",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "8ef3f41ab50cdfa99033",
+    "url": "/Pokedex/static/css/2.30c2d978.chunk.css"
+  },
+  {
+    "revision": "787fa46906f292c79729",
+    "url": "/Pokedex/static/css/main.0115a4c1.chunk.css"
+  },
+  {
+    "revision": "8ef3f41ab50cdfa99033",
+    "url": "/Pokedex/static/js/2.159f9443.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Pokedex/static/js/2.159f9443.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "787fa46906f292c79729",
+    "url": "/Pokedex/static/js/main.fc54502d.chunk.js"
+  },
+  {
+    "revision": "9fb1140b615de1adf3f4",
+    "url": "/Pokedex/static/js/runtime-main.e9efafc6.js"
+  }
+]);
