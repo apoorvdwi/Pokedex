2fe350cb0c9bac7717ff634aaf4008de0f68d695
apoorvdwi <apoorvd14@gmail.com>
2021-02-22 00:07:16 +0530

    Updates

---
+++ precache-manifest.4982d537b65b60a528b8f722fbb11b5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "249eda6c6bca47bc4189ae7cb7a448cb",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "012dd3c60a6b8fa4219f",
+    "url": "/Pokedex/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "6c68a5cb3cd2fd78de1e",
+    "url": "/Pokedex/static/css/main.0eed5966.chunk.css"
+  },
+  {
+    "revision": "012dd3c60a6b8fa4219f",
+    "url": "/Pokedex/static/js/2.da9d77b5.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/Pokedex/static/js/2.da9d77b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c68a5cb3cd2fd78de1e",
+    "url": "/Pokedex/static/js/main.d1d61ec7.chunk.js"
+  },
+  {
+    "revision": "9fb1140b615de1adf3f4",
+    "url": "/Pokedex/static/js/runtime-main.e9efafc6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Pokedex/precache-manifest.2f4fe26767d3692599d6872408a93114.js"
+  "/Pokedex/precache-manifest.4982d537b65b60a528b8f722fbb11b5a.js"
 );
 
 self.addEventListener('message', (event) => {
